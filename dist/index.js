c9355f95fafb1d6dff7674487c8586db75a2a5d3
RomanRybachuk <roman.rybachuk.work@gmail.com>
2025-04-23 18:37:28 +0300

    built

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RegisterUserSchema = exports.updateUserSchema = exports.createUserSchema = void 0;
 const zod_1 = require("zod");
-// import {} from "../dist";
 exports.createUserSchema = zod_1.z.object({
     username: zod_1.z.string().min(1, { message: "Must contain at least 1 character" }),
     email: zod_1.z.string().email({ message: "Must be a valid email address" }),
