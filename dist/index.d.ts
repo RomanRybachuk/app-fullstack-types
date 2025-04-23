9f0da505d404891cb6b04c8a6670f3ec56317bc5
RomanRybachuk <roman.rybachuk.work@gmail.com>
2025-04-23 18:36:27 +0300

    init

---
+++ dist/index.d.ts
@@ -0,0 +1,68 @@
+import { z } from "zod";
+export declare const createUserSchema: z.ZodObject<{
+    username: z.ZodString;
+    email: z.ZodString;
+    password: z.ZodString;
+    refreshToken: z.ZodOptional<z.ZodString>;
+    firstname: z.ZodOptional<z.ZodString>;
+    lastname: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    username: string;
+    email: string;
+    password: string;
+    refreshToken?: string | undefined;
+    firstname?: string | undefined;
+    lastname?: string | undefined;
+}, {
+    username: string;
+    email: string;
+    password: string;
+    refreshToken?: string | undefined;
+    firstname?: string | undefined;
+    lastname?: string | undefined;
+}>;
+export declare const updateUserSchema: z.ZodObject<{
+    username: z.ZodOptional<z.ZodString>;
+    email: z.ZodOptional<z.ZodString>;
+    password: z.ZodOptional<z.ZodString>;
+    refreshToken: z.ZodOptional<z.ZodOptional<z.ZodString>>;
+    firstname: z.ZodOptional<z.ZodOptional<z.ZodString>>;
+    lastname: z.ZodOptional<z.ZodOptional<z.ZodString>>;
+}, "strip", z.ZodTypeAny, {
+    username?: string | undefined;
+    email?: string | undefined;
+    password?: string | undefined;
+    refreshToken?: string | undefined;
+    firstname?: string | undefined;
+    lastname?: string | undefined;
+}, {
+    username?: string | undefined;
+    email?: string | undefined;
+    password?: string | undefined;
+    refreshToken?: string | undefined;
+    firstname?: string | undefined;
+    lastname?: string | undefined;
+}>;
+export declare const RegisterUserSchema: z.ZodObject<Omit<{
+    username: z.ZodString;
+    email: z.ZodString;
+    password: z.ZodString;
+    refreshToken: z.ZodOptional<z.ZodString>;
+    firstname: z.ZodOptional<z.ZodString>;
+    lastname: z.ZodOptional<z.ZodString>;
+}, "refreshToken">, "strip", z.ZodTypeAny, {
+    username: string;
+    email: string;
+    password: string;
+    firstname?: string | undefined;
+    lastname?: string | undefined;
+}, {
+    username: string;
+    email: string;
+    password: string;
+    firstname?: string | undefined;
+    lastname?: string | undefined;
+}>;
+export type CreateUserInput = z.infer<typeof createUserSchema>;
+export type UpdateUserInput = z.infer<typeof updateUserSchema>;
+export type RegisterUserInput = z.infer<typeof RegisterUserSchema>;
---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RegisterUserSchema = exports.updateUserSchema = exports.createUserSchema = void 0;
+const zod_1 = require("zod");
+// import {} from "../dist";
+exports.createUserSchema = zod_1.z.object({
+    username: zod_1.z.string().min(1, { message: "Must contain at least 1 character" }),
+    email: zod_1.z.string().email({ message: "Must be a valid email address" }),
+    password: zod_1.z
+        .string()
+        .min(8, { message: "Must contain at least 8 characters" }),
+    refreshToken: zod_1.z.string().optional(),
+    firstname: zod_1.z.string().optional(),
+    lastname: zod_1.z.string().optional(),
+});
+exports.updateUserSchema = exports.createUserSchema.partial();
+exports.RegisterUserSchema = exports.createUserSchema.omit({ refreshToken: true });
